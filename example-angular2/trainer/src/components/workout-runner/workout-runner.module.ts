be1be45bb2d237e2e5de6bf378b020be44300886
raoul2000 <manu34@free.fr>
2017-04-20 09:51:02 +0200

    fix dep and add audio wip

--- example-angular2/trainer/src/components/workout-runner/workout-runner.module.ts
+++ example-angular2/trainer/src/components/workout-runner/workout-runner.module.ts
@@ -6,15 +6,18 @@ import {ExerciseDescriptionComponent} from './exercise-description/exercise-desc
 import {VideoPlayerComponent} from './video-player/video-player.component';
 import {VideoDialogComponent} from './video-player/video-dialog.component';
 import {SecondsToTimePipe} from './seconds-to-time.pipe';
+import { MyAudioDirective } from './workout-audio/my-audio.directive';
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [
-        WorkoutRunnerComponent, 
-        ExerciseDescriptionComponent, 
+        WorkoutRunnerComponent,
+        ExerciseDescriptionComponent,
         VideoPlayerComponent,
-        VideoDialogComponent, 
-        SecondsToTimePipe],
+        VideoDialogComponent,
+        SecondsToTimePipe,
+        MyAudioDirective
+    ],
     exports: [WorkoutRunnerComponent],
     entryComponents:[VideoDialogComponent]
 })
