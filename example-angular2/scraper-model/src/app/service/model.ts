46c4728516df1ff3347a32f7ae6d33759c27a878
raoul2000 <manu34@free.fr>
2017-07-03 11:54:23 +0200

    add node Id (uuid)

--- example-angular2/scraper-model/src/app/service/model.ts
+++ example-angular2/scraper-model/src/app/service/model.ts
@@ -1,65 +1,71 @@
+import { UUID } from 'angular2-uuid';
+
 export class DocumentModel {
-  private rootNode:NodeModel;
+  private rootNode: NodeModel;
 
   constructor() {
     this.rootNode = new NodeModel('root');
   }
 
-  getRootNode():NodeModel { return this.rootNode;}
+  getRootNode(): NodeModel { return this.rootNode; }
 
 }
 
 export class NodeModel {
+  private id: string;
   private children: Array<NodeModel> = [];
+  private ownerDocument:DocumentModel;
   private expanded: boolean = true;
-  private selected:boolean = false;
+  private selected: boolean = false;
 
   constructor(
     private name,
-    private selector?:string,
-    private type?:string,
+    private selector?: string,
+    private type?: string,
     private metadata?: any
-  ) { }
+  ) {
+    this.id = UUID.UUID();
+  }
 
-  getName():string     {return this.name;  }
-  getSelector():string     {return this.selector;  }
-  setSelector(sel:string)  { this.selector = sel;  }
+  getId():string            { return this.id; }
+  getName(): string         { return this.name; }
 
-  getType():string    {return this.type;  }
-  setType(type:string)  { this.type = type;  }
+  getSelector(): string     { return this.selector; }
+  setSelector(sel: string)  { this.selector = sel; }
+
+  getType(): string         { return this.type; }
+  setType(type: string)     { this.type = type; }
 
 
-  getMetadata():any {return this.metadata; }
+  getMetadata(): any        { return this.metadata; }
+  setMetadata(metadata):any { this.metadata = metadata; }
 
-  getChildren():Array<NodeModel> { return this.children; }
-  hasChildren():boolean { return this.children.length !== 0 ; }
-  isExpanded():boolean  { return this.expanded; }
-  toggle() :void     { this.expanded = !this.expanded; }
-  addChild(node: NodeModel):NodeModel {
+  getChildren(): Array<NodeModel> { return this.children; }
+  hasChildren(): boolean { return this.children.length !== 0; }
+  isExpanded(): boolean { return this.expanded; }
+  toggle(): void { this.expanded = !this.expanded; }
+  addChild(node: NodeModel): NodeModel {
     this.children.push(node);
     return this;
   }
 
-  isSelected():boolean  { return this.selected; }
-  select(node: NodeModel):void   {
-    if( this.name === node.name) {
-      this.selected = true;
-    } else {
-      this.selected = false;
-    }
+  isSelected(): boolean { return this.selected; }
+  select(node: NodeModel): void {
+    this.selected = this.getId() === node.getId();
     this.children.forEach(n => n.select(node));
   }
   /**
-   * Mark this node and all its children as unselected
+   * Mark this node and all its children as NOT selected
    */
   deselect() {
     this.selected = false;
     this.children.forEach(n => n.deselect());
   }
-  removeChild(child:NodeModel) {
-    let idx = this.children.findIndex(x => x.getName() === child.getName());
-    if( idx > -1 ) {
-      this.children.splice(idx,1);
+
+  removeChild(child: NodeModel) {
+    let idx = this.children.findIndex(x => x.getId() === child.getId());
+    if (idx > -1) {
+      this.children.splice(idx, 1);
     }
   }
 }
