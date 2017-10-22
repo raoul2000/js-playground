3098cc46114c14094c2bc60599ae837232408fab
raoul2000 <manu34@free.fr>
2017-10-22 23:10:38 +0200

    todo (wip)

--- example-elm/play-1/my-todo.elm
+++ example-elm/play-1/my-todo.elm
@@ -1,19 +1,21 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 type alias Task = { name : String, completed : Bool }
-type alias TaskList = List Task
+type alias TaskList = { list : List Task}
 
 type Msg
   = None
+  | NewTask String
 
 init : (TaskList, Cmd Msg)
 init = (
-   [
-    Task "buy milk"  True,
+   { list = [
+    Task  "buy milk"  True,
     Task "buy bread"  False
-  ], Cmd.none)
+  ]}, Cmd.none)
 
 
 isCompleted : Task -> Bool
@@ -25,6 +27,9 @@ update msg model =
   case msg of
     None ->
       (model, Cmd.none)
+    NewTask name ->
+      (model, Cmd.none)
+--      ( { name = name, completed = False } :: model.list , Cmd.none)
 
 
 view : TaskList -> Html Msg
@@ -34,13 +39,15 @@ view  taskList =
     h1 [] [ text "task list"]
   ,  hr [] []
   ,  renderTaskList taskList
+  ,  input [ placeholder "Text to reverse", onInput NewTask ] []
+  ,  button [] [ text "new task" ]
   ]
 
 
 renderTaskList : TaskList -> Html Msg
 renderTaskList taskList =
   ul [] (
-    List.map renderSingleTask taskList
+    List.map renderSingleTask taskList.list
   )
 
 renderSingleTask : Task -> Html Msg
