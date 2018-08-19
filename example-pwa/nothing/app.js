075b6ec17661be2adac86f34f12e10f131bcdf2a
raoul2000 <manu34@free.fr>
2018-08-19 18:01:00 +0200

    add in-page logger

--- example-pwa/nothing/app.js
+++ example-pwa/nothing/app.js
@@ -3,23 +3,42 @@ console.log('loading app.js');
 var app = {
     installPromptEvent : null,
     btnInstall : null,
+    logger:null,
     run : function() {
         console.log('running App');
+        this.logger = document.querySelector('#logger');
+        this.log("initializing the Nothing App");
+
         this.btnInstall = document.querySelector('#install');
         this.registerServiceWorker();
         this.registerCustomInstaller();
     },
+    log : function(msg) {
+        console.log(msg);
+        var newEntry = document.createElement('div');
+        newEntry.textContent = '> ' + msg;
+
+        var firstEntry = this.logger.querySelector('.entry');
+        if(firstEntry) {
+            this.logger.insertBefore(newEntry,firstEntry);
+        } else {
+            this.logger.appendChild(newEntry);
+        }
+    },
     registerServiceWorker : function() {
+        var that = this;
+        this.log('trying to register ServiceWorker');
         if ('serviceWorker' in navigator) {
+            this.log('ServiceWorker is supported : registering my own');
             navigator.serviceWorker
                 .register('./sw.js')
                 .then(function () {
-                console.log('Service Worker Registered For Nothing');
+                    that.log('Service Worker Registered For Nothing');
             });
         }    
     },
     registerCustomInstaller : function() {
-        console.log('installing custom install handler');
+        this.log('installing custom install handler');
         var that = this;
         window.addEventListener('beforeinstallprompt', function (event) {
             event.preventDefault();
@@ -32,9 +51,9 @@ var app = {
             that.installPromptEvent.prompt();
             that.installPromptEvent.userChoice.then((choice) => {
                 if (choice.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt');
+                    that.log('User accepted the A2HS prompt');
                 } else {
-                    console.log('User dismissed the A2HS prompt');
+                    that.log('User dismissed the A2HS prompt');
                 }
                 that.installPromptEvent = null;
             });
