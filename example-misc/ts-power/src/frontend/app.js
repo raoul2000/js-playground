e81259f93041cbb36ef8d76b3c15a0898bbb3d5b
raoul2000 <manu34@free.fr>
2018-12-18 11:00:05 +0100

    tagify : custom tag selector impl

--- example-misc/ts-power/src/frontend/app.js
+++ example-misc/ts-power/src/frontend/app.js
@@ -3,16 +3,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     let tagify = null;
+    let tagList = [];
+
     fetch('http://127.0.0.1:5454/api/tags').
         then((RES) => {
             if( RES.ok ) {
-                RES.json().then( (whitelist) => {
+                RES.json().then( (tags) => {
+                    tagList = tags;
                     tagify = new Tagify(document.querySelector('input[name=tags]'), {
-                        "whitelist": whitelist.map( (tag) => tag.name),
+                        "whitelist": [],
                         "dropdown" : {
                             "enabled"   : 1
                         }
-                    });           
+                    }); 
+                    tagify.on('input', function(e) {
+                        console.log(e);
+                        tagify.settings.whitelist = tagList.map( (tag) => tag.name)
+                            .filter( (tagName) => tagName.toLowerCase().indexOf(e.detail.value.toLowerCase()) >= 0);
+
+                        // eslint-disable-next-line prefer-reflect
+                        tagify.dropdown.show.call(tagify);
+                        
+                    });
                 });
             }
         }).
