85ab286dfc0b496e222e92cca0b0a908e0e6c722
raoul2000 <manu34@free.fr>
2019-05-12 16:40:05 +0200

    add vuetify sample app

---
+++ example-vue/vuetify-1/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-toolbar app class="elevation-1">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>MyApp</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list class="pa-1">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+
+        <v-list-tile v-for="item in items" :key="item.title" :to="{path: item.link}" @click>  
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view>
+        <HelloWorld/>
+      </router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Home", icon: "dashboard", link :"/" },
+        { title: "List", icon: "list", link : "/list"},
+        { title: "Settings", icon: "games", link : "/settings"},
+        { title: "About", icon: "question_answer", link : "/about"}
+      ]
+    };
+  }
+};
+</script>
