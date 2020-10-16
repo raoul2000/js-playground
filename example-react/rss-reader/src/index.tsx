8a79a48c7cc2017d3b32ca0c376828af9e9a9ce3
raoul2000 <manu34@free.Fr>
2020-10-16 12:22:52 +0200

    add sample rss

--- example-react/rss-reader/src/index.tsx
+++ example-react/rss-reader/src/index.tsx
@@ -8,24 +8,24 @@ import { addRssSource } from './store/rss-source/actions';
 
 [
   {
-    id: 'une-inter',
-    label: 'La Une International',
-    url: 'https://www.lemonde.fr/international/rss_full.xml'
+    id: 'une-societe',
+    label: 'La Une Societe',
+    url: 'http://localhost:3000/test-response/rss_full_societe.xml'
   },
   {
-    id: 'une-fr',
-    label: 'La Une France',
-    url: 'http://localhost:3000/test-response/une.xml'
+    id: 'une-planete',
+    label: 'La Une Planete',
+    url: 'http://localhost:3000/test-response/rss_full_planete.xml'
   },
   {
     id: 'une-sport',
     label: 'La Une Sport',
-    url: 'https://www.lemonde.fr/rss/sport.xml'
+    url: 'http://localhost:3000/test-response/rss_full_sport.xml'
   },
   {
     id: 'une-politique',
     label: 'La Une Politique',
-    url: 'https://www.lemonde.fr/rss/sport.xml'
+    url: 'http://localhost:3000/test-response/rss_full_politique.xml'
   },
   {
     id: 'une-div',
