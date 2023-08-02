9c813f5d411c07d117b84ca760a7003274435a87
raoul2000 <manu34@free.fr>
2023-08-02 17:34:39 +0200

    auto-complete: improve options

--- example-auto-complete/tags-input.js
+++ example-auto-complete/tags-input.js
@@ -1,4 +1,6 @@
 window.tagsInputAutoComplete = (function () {
+    console.log("loading tagsInputAutoComplete plugin ...");
+
     // Helpers /////////////////////////////////////////////////////////////////////
 
     const removeAllChildren = (element) => {
@@ -6,8 +8,7 @@ window.tagsInputAutoComplete = (function () {
             element.removeChild(element.firstChild);
         }
     };
-    const normalizeSeed = (seed) =>
-        typeof seed === "string" ? seed.trim() : "";
+    const normalizeSeed = (seed) => (typeof seed === "string" ? seed.trim() : "");
 
     // Domain /////////////////////////////////////////////////////////////////////
 
@@ -16,114 +17,88 @@ window.tagsInputAutoComplete = (function () {
         textInputElement.style.width = ghostElement.offsetWidth + "px";
     };
 
-    const updateOptionListSpec = (sourceElement, targetElement) => {
+    const alignElements = (sourceElement, targetElement) => {
         const cs = getComputedStyle(sourceElement);
 
         targetElement.style.width = "";
-        targetElement.style.borderBottomWidth = cs.getPropertyValue(
-            "border-bottom-width"
-        );
-        targetElement.style.borderTopWidth =
-            cs.getPropertyValue("border-top-width");
-        targetElement.style.borderLeftWidth =
-            cs.getPropertyValue("border-left-width");
-        targetElement.style.borderRightWidth =
-            cs.getPropertyValue("border-right-width");
-        targetElement.style.paddingBottom =
-            cs.getPropertyValue("padding-bottom");
+        targetElement.style.borderBottomWidth = cs.getPropertyValue("border-bottom-width");
+        targetElement.style.borderTopWidth = cs.getPropertyValue("border-top-width");
+        targetElement.style.borderLeftWidth = cs.getPropertyValue("border-left-width");
+        targetElement.style.borderRightWidth = cs.getPropertyValue("border-right-width");
+        targetElement.style.paddingBottom = cs.getPropertyValue("padding-bottom");
         targetElement.style.paddingTop = cs.getPropertyValue("padding-top");
         targetElement.style.paddingLeft = cs.getPropertyValue("padding-left");
         targetElement.style.paddingRight = cs.getPropertyValue("padding-right");
-        targetElement.style.left = `-${cs.getPropertyValue(
-            "border-left-width"
-        )}`;
-        targetElement.style.right = `-${cs.getPropertyValue(
-            "border-right-width"
-        )}`;
+        targetElement.style.left = `-${cs.getPropertyValue("border-left-width")}`;
+        targetElement.style.right = `-${cs.getPropertyValue("border-right-width")}`;
         targetElement.style.top = `${sourceElement.clientHeight}px`;
     };
 
     const removeAllOptions = removeAllChildren;
-    const hideOptionList = (optionListElement) =>
-        (optionListElement.style.display = "none");
-    const showOptionList = (optionListElement) =>
-        (optionListElement.style.display = "block");
-    const countOptions = (optionListElement) =>
-        optionListElement.children.length;
-    const optionListNotEmpty = (optionListElement) =>
-        countOptions(optionListElement) > 0;
-
+    const hideOptionList = (optionListElement) => (optionListElement.style.display = "none");
+    const showOptionList = (optionListElement) => (optionListElement.style.display = "block");
+    const countOptions = (optionListElement) => optionListElement.children.length;
+    const optionListNotEmpty = (optionListElement) => countOptions(optionListElement) > 0;
+    const readValue = (element) => JSON.parse(element.dataset.value);
+    const writeValue = (element, value) => (element.dataset.value = JSON.stringify(value));
     const resetOptionList = (optionListElement) => {
         hideOptionList(optionListElement);
         removeAllOptions(optionListElement);
     };
 
-    const newOptionElementCreator = (readOptionLabel) => (option) => {
+    const newOptionElementCreator = (readOptionLabel) => (optionValue) => {
         const elOption = document.createElement("div");
-        elOption.textContent = readOptionLabel(option);
-        elOption.dataset.value = JSON.stringify(option);
+        elOption.textContent = readOptionLabel(optionValue);
+        writeValue(elOption, optionValue);
         return elOption;
     };
 
-    const createTagElement = (textContent, dataSetValue) => {
+    const createTagElement = (textContent, value) => {
         const tag = document.createElement("div");
         tag.classList.add("tag");
         tag.textContent = textContent;
-        if (dataSetValue) {
-            tag.dataset.value = dataSetValue;
+        if (value) {
+            writeValue(tag, value);
         }
         return tag;
     };
 
     const createTagElementFromOptionElement = (optionElement) =>
-        createTagElement(
-            optionElement.textContent,
-            optionElement.dataset.value
-        );
+        createTagElement(optionElement.textContent, readValue(optionElement));
+
+    const getSelectedTagElements = (elTagsInput) => Array.from(elTagsInput.querySelectorAll(".tag"));
 
     const getSelectedTagValues = (elTagsInput) =>
-        Array.from(elTagsInput.querySelectorAll(".tag")).map((tag) =>
-            JSON.parse(tag.dataset.value)
-        );
+        getSelectedTagElements(elTagsInput).map((tagElement) => readValue(tagElement));
 
-    const setSelectedTags = (
-        options,
-        elTagsInput,
-        elTextInput,
-        readOptionLabel
-    ) =>
-        options
-            .map((option) =>
-                createTagElement(
-                    readOptionLabel(option),
-                    JSON.stringify(option)
-                )
-            )
-            .forEach((tagElement) =>
-                elTagsInput.insertBefore(tagElement, elTextInput)
-            );
+    const setSelectedTags = (optionValues, elTagsInput, elTextInput, readOptionLabel) =>
+        optionValues
+            .map((optionValue) => createTagElement(readOptionLabel(optionValue), optionValue))
+            .forEach((tagElement) => elTagsInput.insertBefore(tagElement, elTextInput));
 
     const getActiveOptionElement = (optionListElement) =>
         optionListElement.getElementsByClassName("active")[0];
 
-    const renderOptionList = (
-        elList,
-        options,
-        elTagsInput,
-        createOptionElement
-    ) => {
+    const renderOptionList = (elList, options, elTagsInput, createOptionElement) => {
         resetOptionList(elList);
         if (options.length !== 0) {
             options.map(createOptionElement).forEach((elOption) => {
                 elList.appendChild(elOption);
             });
-            updateOptionListSpec(elTagsInput, elList);
+            alignElements(elTagsInput, elList);
             elList.style.top = `${elTagsInput.clientHeight}px`;
             showOptionList(elList);
         }
     };
 
     const Key = {
+        code: {
+            ARROW_DOWN: 40,
+            ARROW_UP: 38,
+            ENTER: 13,
+            BACKSPACE: 8,
+            ESCAPE: 27,
+        },
         selectionDown: (elOptions) => {
             const activeOption = getActiveOptionElement(elOptions);
             if (!activeOption) {
@@ -145,39 +120,42 @@ window.tagsInputAutoComplete = (function () {
             scrollToSelection(elOptions);
         },
         escape: hideOptionList,
-        enter: (elOptions, elTextInput, elTagsInput, optionComparator) => {
+        enter: (elOptions, elTextInput, elTagsInput, optionComparator, onDuplicateTag) => {
             const activeOption = getActiveOptionElement(elOptions);
             if (activeOption) {
                 if (optionComparator) {
                     // duplicate not allowed
-                    const currentValues = getSelectedTagValues(elTagsInput);
-                    if (currentValues.length !== 0) {
-                        const candidateOption = JSON.parse(
-                            activeOption.dataset.value
+
+                    const currentTagElements = getSelectedTagElements(elTagsInput);
+
+                    if (currentTagElements.length !== 0) {
+                        const candidateOptionValue = readValue(activeOption);
+
+                        const duplicateTags = currentTagElements.filter(
+                            (tagElement) =>
+                                optionComparator(candidateOptionValue, readValue(tagElement)) === 0
                         );
-                        const duplicateCount = currentValues.filter(
-                            (optVal) =>
-                                optionComparator(optVal, candidateOption) === 0
-                        ).length;
-                        if (duplicateCount !== 0) {
-                            console.log("duplicate");
+
+                        if (duplicateTags.length !== 0) {
+                            if (onDuplicateTag) {
+                                onDuplicateTag(duplicateTags[0]);
+                            }
                             resetOptionList(elOptions);
                             return;
                         }
                     }
                 }
-                elTagsInput.insertBefore(
-                    createTagElementFromOptionElement(activeOption),
-                    elTextInput
-                );
+                elTagsInput.insertBefore(createTagElementFromOptionElement(activeOption), elTextInput);
                 elTextInput.value = "";
                 resetOptionList(elOptions);
+                return readValue(activeOption);
             }
         },
         backspace: (elTextInput, elTagsInput) => {
-            const tagToRemove = elTextInput.previousElementSibling;
-            if (elTextInput.value.length === 0 && tagToRemove) {
-                elTagsInput.removeChild(tagToRemove);
+            const tagToRemoveElement = elTextInput.previousElementSibling;
+            if (elTextInput.value.length === 0 && tagToRemoveElement) {
+                elTagsInput.removeChild(tagToRemoveElement);
+                return readValue(tagToRemoveElement);
             }
         },
     };
@@ -186,18 +164,13 @@ window.tagsInputAutoComplete = (function () {
         const activeOptionElement = getActiveOptionElement(elOptions);
         if (activeOptionElement) {
             elOptions.scrollTop =
-                activeOptionElement.offsetTop -
-                elOptions.clientHeight +
-                activeOptionElement.clientHeight;
+                activeOptionElement.offsetTop - elOptions.clientHeight + activeOptionElement.clientHeight;
         }
     };
 
     // Event Handlers ////////////////////////////////////////////////////////////////////
 
-    const registerEventHandlers = (
-        { tagsInput, textInput, textInputGhost, optionList },
-        ctx
-    ) => {
+    const registerEventHandlers = ({ tagsInput, textInput, textInputGhost, optionList }, ctx) => {
         /* input text ---------------------------- */
         textInput.addEventListener("input", (ev) => {
             resizeTextInput(textInput, textInputGhost);
@@ -222,24 +195,31 @@ window.tagsInputAutoComplete = (function () {
                 const key = ev.keyCode || ev.charCode;
 
                 switch (key) {
-                    case 40: // Arrow DOWN
+                    case Key.code.ARROW_DOWN:
                         Key.selectionDown(optionList);
                         break;
-                    case 38: // Arrow UP
+                    case Key.code.ARROW_UP:
                         Key.selectionUp(optionList);
                         break;
-                    case 13: // Enter
-                        Key.enter(
+                    case Key.code.ENTER:
+                        const valueAdded = Key.enter(
                             optionList,
                             textInput,
                             tagsInput,
-                            ctx.optionComparator
+                            ctx.optionComparator,
+                            ctx.onDuplicateTag
                         );
+                        if (valueAdded && ctx.onTaglistChange) {
+                            ctx.onTaglistChange(valueAdded, true);
+                        }
                         break;
-                    case 8: // Backspace
-                        Key.backspace(textInput, tagsInput);
+                    case Key.code.BACKSPACE:
+                        const valueRemoved = Key.backspace(textInput, tagsInput);
+                        if (valueRemoved && ctx.onTaglistChange) {
+                            ctx.onTaglistChange(valueRemoved, false);
+                        }
                         break;
-                    case 27: // ESC
+                    case Key.code.ESCAPE:
                         hideOptionList(optionList);
                         break;
                 }
@@ -258,10 +238,7 @@ window.tagsInputAutoComplete = (function () {
         let optionListHideTimer;
 
         textInput.addEventListener("focusout", (ev) => {
-            optionListHideTimer = setTimeout(
-                () => hideOptionList(optionList),
-                200
-            );
+            optionListHideTimer = setTimeout(() => hideOptionList(optionList), 200);
         });
 
         /* option list  ---------------------------- */
@@ -275,12 +252,16 @@ window.tagsInputAutoComplete = (function () {
                     optionListHideTimer = null;
                 }
                 ev.target.classList.add("active");
-                Key.enter(
+                const valueAdded = Key.enter(
                     optionList,
                     textInput,
                     tagsInput,
-                    ctx.optionComparator
+                    ctx.optionComparator,
+                    ctx.onDuplicateTag
                 );
+                if (valueAdded && ctx.onTaglistChange) {
+                    ctx.onTaglistChange(valueAdded, true);
+                }
             },
             false
         );
@@ -341,10 +322,8 @@ window.tagsInputAutoComplete = (function () {
 
         const textInputCS = getComputedStyle(textInput);
         textInputGhost.style.padding = textInputCS.getPropertyValue("padding");
-        textInputGhost.style.fontSize =
-            textInputCS.getPropertyValue("font-size");
-        textInputGhost.style.fontFamily =
-            textInputCS.getPropertyValue("font-family");
+        textInputGhost.style.fontSize = textInputCS.getPropertyValue("font-size");
+        textInputGhost.style.fontFamily = textInputCS.getPropertyValue("font-family");
 
         return {
             tagsInput,
@@ -353,6 +332,7 @@ window.tagsInputAutoComplete = (function () {
             optionList,
         };
     };
+    const nullFunction = () => {};
     const identity = (v) => v;
     const defaultOptionFilter = (txt, option) => option.startsWith(txt);
     const defaultOptionLabel = identity;
@@ -367,12 +347,18 @@ window.tagsInputAutoComplete = (function () {
             return def;
         }
     };
+    const defaultOnDuplicateTag = (tagElement) => {
+        tagElement.classList.add("blink");
+        tagElement.addEventListener("animationend", (ev) => tagElement.classList.remove("blink"), {
+            once: true,
+        });
+    };
     const defaultOptionComparator = (a, b) => {
         if (a === b) return 0;
         if (a < b) return -1;
         return 1;
     };
-    const nullComparator = (a, b) => 1;
+
     /**
      * Initialize the control.
      *
@@ -385,6 +371,9 @@ window.tagsInputAutoComplete = (function () {
      * in the input text element. Signature is (string, stinrg | object) => boolean
      * - `allowDuplicate` : (boolean - default = "*true*")
      * - `optionComparator`: (optional) - custom option comparator function (see https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
+     * - `onDuplicateTag`: (optional) - function invoked when a tag is duplicated. Receive the DOM elemeent reprseenting the tag
+     * - `onTaglistChange`: (optional) - function invoked when the list of selected tags changes. It accept 2 arguments *value* the tag value added or removed
+     * and *isAdded* a boolean value set to TRUE when a tag has been selected or FALSE when a tag has been deselected (removed)
      *
      * @param {object | string } config when string Id of the DOM element that will hold the tas input control. When Object, the DOM element itself
      * @param {object} config configuration object
@@ -399,6 +388,8 @@ window.tagsInputAutoComplete = (function () {
             optionFilter,
             allowDuplicate,
             optionComparator,
+            onDuplicateTag,
+            onTaglistChange,
         }
     ) => {
         // Process configuration
@@ -436,10 +427,20 @@ window.tagsInputAutoComplete = (function () {
 
         const ctx = {
             optionLabel: optLabel,
-            optionFilter: (txt) =>
-                options.filter((option) => optFilter(txt, option)),
+            optionFilter: (txt) => options.filter((option) => optFilter(txt, option)),
             optionComparator: optComparator,
+            onDuplicateTag: validateFunctionOrDefault(
+                onDuplicateTag,
+                defaultOnDuplicateTag,
+                "onDuplicateTag must be a function"
+            ),
+            onTaglistChange: validateFunctionOrDefault(
+                onTaglistChange,
+                () => {},
+                "onTaglistChange must be a function"
+            ),
         };
+
         ctx.createOptionElement = newOptionElementCreator(ctx.optionLabel);
 
         // update the DOM : inject control elements
@@ -449,13 +450,9 @@ window.tagsInputAutoComplete = (function () {
 
         // update control with provided initial options
         if (Array.isArray(initialOptions)) {
-            setSelectedTags(
-                initialOptions,
-                UI.tagsInput,
-                UI.textInput,
-                ctx.optionLabel
-            );
+            setSelectedTags(initialOptions, UI.tagsInput, UI.textInput, ctx.optionLabel);
         }
+
         // refresh text input size
         resizeTextInput(UI.textInput, UI.textInputGhost);
 
